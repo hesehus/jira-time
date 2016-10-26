28ba98dae223f861b5471ad6c8f8e2b549e17acb
hkhesehus <hk@hesehus.dk>
2016-10-26 20:57:16 +0200

    Added button to notify change to app

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { IndexLink, Link } from 'react-router'
 import './Header.scss'
 import ListIcon from './assets/list.svg';
 import UserIcon from './assets/user.svg';
 
-export const Header = () => (
-  <div className='header'>
-    <IndexLink to='/jira-time' className='header__route' activeClassName='header__route--active'>
-      <img className='header__route__icon' src={ListIcon} alt='Home' />
-    </IndexLink>
-    <Link to='/jira-time/profile' className='header__route' activeClassName='header__route--active'>
-      <img className='header__route__icon' src={UserIcon} alt='Profile' />
-    </Link>
-  </div>
-)
+export default class Header extends Component {
 
-export default Header
+  constructor (props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentWillMount () {
+    if (window.__getServiceWorkerStatus) {
+      window.__getServiceWorkerStatus()
+        .then((status) => {
+          this.setState({ serviceWorkerUpdated: status.updateAvailable });
+        });
+    }
+  }
+
+  render () {
+    return (
+      <div className='header'>
+        <IndexLink to='/jira-time' className='header__route' activeClassName='header__route--active'>
+          <img className='header__route__icon' src={ListIcon} alt='Home' />
+        </IndexLink>
+        <Link to='/jira-time/profile' className='header__route' activeClassName='header__route--active'>
+          <img className='header__route__icon' src={UserIcon} alt='Profile' />
+        </Link>
+        {this.state.serviceWorkerUpdated ? <div>Update available!</div> : null}
+      </div>
+    );
+  }
+}
