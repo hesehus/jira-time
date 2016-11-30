7cbac22e5d7c01fc2c23b6acd7694af803b0adf9
Anders Fjeldsø Christiansen <afc@hesehus.dk>
2016-11-30 14:59:11 +0100

    removed chromeTabs

--- src/modules/Tabs/components/Tabs.js
+++ src/modules/Tabs/components/Tabs.js
@@ -14,46 +14,14 @@ export default class Tabs extends Component {
 
     super();
 
-    debugger;
-
-    //chromeTabs = chromeTabs.default
-
-    // Add chrome tabs CSS to page head
-    chromeTabs.insertCss()
-
-    // Init chrome tabs in the container .chrome-tabs-shell
-    var $chromeTabsExampleShell = $(".chrome-tabs-shell")
-
-    chromeTabs.init({
-      $shell: $chromeTabsExampleShell,
-      minWidth: 45,
-      maxWidth: 180
-    })
 
   }
 
   render () {
     return (
-          <div class="chrome-tabs">
-            <div class="chrome-tabs-content">
-              <div class="chrome-tab">
-                <div class="chrome-tab-background">
-                </div>
-                <div class="chrome-tab-favicon" style="background-image: url('demo/images/google-favicon.png')"></div>
-                <div class="chrome-tab-title">Google</div>
-                <div class="chrome-tab-close"></div>
-              </div>
-              <div class="chrome-tab chrome-tab-current">
-                <div class="chrome-tab-background">
-                </div>
-                <div class="chrome-tab-favicon" style="background-image: url('demo/images/facebook-favicon.ico')"></div>
-                <div class="chrome-tab-title">Facebook</div>
-                <div class="chrome-tab-close"></div>
-              </div>
-            </div>
-            <div class="chrome-tabs-bottom-bar"></div>
-          </div>
-
+        <div class="tabs">
+            tabs
+        </div>
     );
   }
 }
