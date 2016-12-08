a92c2f59a76fb75952bf4a98ffbc5c503fc61413
hkhesehus <hk@hesehus.dk>
2016-12-08 22:30:26 +0100

    Changed to new time input

--- src/modules/DateInput/components/DateInput.js
+++ src/modules/DateInput/components/DateInput.js
@@ -1,5 +1,6 @@
 import moment from 'moment';
 import React, { Component, PropTypes } from 'react';
+import TimeInput from 'time-input';
 
 // import './DateInput.scss';
 
@@ -15,20 +16,52 @@ export default class DateInput extends Component {
   constructor (props) {
     super(props);
 
-    this.onChange = this.onChange.bind(this);
+    this.onDateChange = this.onDateChange.bind(this);
+    this.onTimeChange = this.onTimeChange.bind(this);
     this.onTodayClick = this.onTodayClick.bind(this);
 
-    this.state = {};
+    this.state = {
+      date: null
+    };
   }
 
-  onChange () {
+  componentWillMount () {
+    const dateObject = moment(this.props.date);
 
+    const date = dateObject.format('YYYY-MM-DD');
+    const time = dateObject.format('HH:mm');
+
+    this.setState({
+      date,
+      time
+    });
+  }
+
+  onTimeChange (time) {
+
+    const vals = time.split(':');
+    const date = new Date(this.state.date);
+    
+    date.setHours(vals[0]);
+    date.setMinutes(vals[1]);
+
+    this.props.onChange({
+      date
+    });
+
+    this.setState({
+      time
+    });
+  }
+
+  onDateChange () {
+    
     let date;
 
     if (this.refs.date) {
       date = new Date(this.refs.date.value + ' ' + this.refs.time.value);
     } else {
-      date = new Date(this.props.date);
+      date = new Date(this.state.date);
 
       const vals = this.refs.time.value.split(':');
       date.setHours(vals[0]);
@@ -36,6 +69,10 @@ export default class DateInput extends Component {
     }
 
     this.props.onChange({
+      date
+    });
+
+    this.setState({
       date
     });
   }
@@ -48,39 +85,32 @@ export default class DateInput extends Component {
 
   render () {
 
+    const { time, date } = this.state;
+
     const className = `record-date record-date--${this.props.type}`;
-
-    const dateObject = moment(this.props.date);
-
-    const date = dateObject.format('YYYY-MM-DD');
-    const time = dateObject.format('HH:mm');
 
     const dateDisplay = (
       <input type='date'
         ref='date'
         defaultValue={date}
-        onChange={this.onChange}
+        onChange={this.onDateChange}
         className='record-date__input record-date__input--date'
         disabled={this.props.disabled}
        />
     );
-    const timeDisplay = (
-      <input type='time'
-        ref='time'
-        defaultValue={time}
-        onChange={this.onChange}
-        className='record-date__input record-date__input--time'
-        disabled={this.props.disabled}
-       />
-    );
 
-    let isToday = moment().isSame(dateObject, 'day');
+    let isToday = moment().isSame(moment(date), 'day');
     let today = <span className='record-date__today' onClick={this.onTodayClick}>Today</span>;
 
     return (
       <span className={className}>
         {isToday && !this.state.showDate ? today : dateDisplay}
-        {timeDisplay}
+        <TimeInput
+          value={time}
+          className='record-date__input record-date__input--time'
+          onChange={this.onTimeChange}
+          disabled={this.props.disabled}
+        />
       </span>
     );
   }
