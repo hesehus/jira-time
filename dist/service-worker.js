ab4cdfa5938ec7fcd5f8afdc7c7f6bbed0ad4699
Håkon Holmsen Krogh <hk@hesehus.dk>
2016-12-19 08:18:54 +0100

    Build

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/jira-time/4a47e47fb02419923df8ca270281c656.jpg","4a47e47fb02419923df8ca270281c656"],["/jira-time/app.css","c0f766e843fb8cb80787334f65a45d89"],["/jira-time/app.js","dcd1826a4d5255d7475bcfd9ba53321f"],["/jira-time/bb85f71a0f5420ac7b12b0e7487521da.png","bb85f71a0f5420ac7b12b0e7487521da"],["/jira-time/favicon.ico","456d2fdfd5ba9137f0bf3100c098e5f1"],["/jira-time/index.html","758967857975d3ecb1978daa31c2c36e"],["/jira-time/vendor.js","bd7def130d0235e4320dc796f474cbf2"]];
+var precacheConfig = [["/jira-time/4a47e47fb02419923df8ca270281c656.jpg","4a47e47fb02419923df8ca270281c656"],["/jira-time/app.css","cb971a879e6608a020fe1a8f3550ae25"],["/jira-time/app.js","bee1cad2ff08808a9d845551137524db"],["/jira-time/bb85f71a0f5420ac7b12b0e7487521da.png","bb85f71a0f5420ac7b12b0e7487521da"],["/jira-time/favicon.ico","456d2fdfd5ba9137f0bf3100c098e5f1"],["/jira-time/index.html","758967857975d3ecb1978daa31c2c36e"],["/jira-time/vendor.js","423a6e32ce6bf0003850449ebcba0e03"]];
 var cacheName = 'sw-precache-v2-jira-time-' + (self.registration ? self.registration.scope : '');
 
 
