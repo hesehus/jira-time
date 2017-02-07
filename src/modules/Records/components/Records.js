cfe329941f0073d9b3a8b59c7c3d4309bd7e0d38
Håkon Holmsen Krogh <hk@hesehus.dk>
2017-02-07 08:45:38 +0100

    Adjusted the record item animations

--- src/modules/Records/components/Records.js
+++ src/modules/Records/components/Records.js
@@ -19,66 +19,68 @@ export default class Records extends Component {
         const { records, taskIndex } = this.props;
 
         return (
-            <div className={'records' + (!!records.length ? '' : ' records--empty')}>
-                <TransitionMotion
-                  willEnter={() => {
-                      return {
-                          opacity: 0,
-                          scale: 0.75,
-                          height: 0
-                      };
-                  }}
-                  willLeave={() => {
-                      return {
-                          opacity: spring(0),
-                          scale: spring(0.75),
-                          height: spring(0)
-                      };
-                  }}
-                  styles={records.map((record, i) => ({
-                      key: `record-${i}`,
-                      style: {
-                          opacity: spring(1),
-                          scale: spring(1),
-                          height: spring(53) // At the moment we have a fixed height of 53px for all record items
-                      },
-                      data: (
-                          <RecordItem
-                            recordCuid={record.cuid}
-                            record={record}
-                            key={record.cuid}
-                            taskIndex={taskIndex}
-                            recordIndex={i}
-                          />
-                      )
-                  }))}
-                >
-                    {interpolatedStyles => {
-                        events.emit('record-item-animate');
+            <TransitionMotion
+              willEnter={() => {
+                  return {
+                      opacity: 0,
+                      scale: 0.9,
+                      height: 0,
+                      marginTop: 0
+                  };
+              }}
+              willLeave={() => {
+                  return {
+                      opacity: spring(0),
+                      scale: spring(0.9),
+                      height: spring(0),
+                      marginTop: spring(0)
+                  };
+              }}
+              styles={records.map((record, i) => ({
+                  key: `record-${record.cuid}`,
+                  style: {
+                      opacity: spring(1),
+                      scale: spring(1),
+                      height: spring(53), // At the moment we have a fixed height of 53px for all record items
+                      marginTop: i === 0 ? spring(10) : spring(5)
+                  },
+                  data: (
+                      <RecordItem
+                        recordCuid={record.cuid}
+                        record={record}
+                        key={record.cuid}
+                        taskIndex={taskIndex}
+                        recordIndex={i}
+                      />
+                  )
+              }))}
+            >
+                {interpolatedStyles => {
+                    events.emit('record-item-animate');
 
-                        return (
-                            <div>
-                                {interpolatedStyles.map((interpolated) => {
-                                    return (
-                                        <div
-                                          key={interpolated.key}
-                                          style={{
-                                              opacity: interpolated.style.opacity,
-                                              WebkitTransform: `scale(${interpolated.style.scale})`,
-                                              transform: `scale(${interpolated.style.scale})`,
-                                              height: `${interpolated.style.height}px`
-                                          }}
-                                          className='record-wrap'
-                                        >
-                                            {interpolated.data}
-                                        </div>
-                                    );
-                                })}
-                            </div>
-                        );
-                    }}
-                </TransitionMotion>
-            </div>
+                    return (
+                        <div className='records'>
+                            {interpolatedStyles.map((interpolated) => {
+                                return (
+                                    <div
+                                      key={interpolated.key}
+                                      style={{
+                                          opacity: interpolated.style.opacity,
+                                          WebkitTransform: `scale(${interpolated.style.scale})`,
+                                          transform: `scale(${interpolated.style.scale})`,
+                                          height: `${interpolated.style.height}px`,
+                                          marginTop: `${interpolated.style.marginTop}px`
+                                      }}
+                                      className='record-wrap'
+                                    >
+                                        {interpolated.data}
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    );
+                }}
+            </TransitionMotion>
         );
     }
 }
